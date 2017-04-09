d48857cb19bb483c8757ce494e8e73661c979106
olexandr <kutoviyo@gmail.com>
2017-04-09 13:58:43 +0300

    Add project

---
+++ src/main/java/server/ProcessConnectionThread.java
@@ -0,0 +1,62 @@
+package server;
+
+import javax.microedition.io.StreamConnection;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.InputStream;
+
+
+public class ProcessConnectionThread implements Runnable {
+
+    private StreamConnection mConnection;
+
+    // Constant that indicate command from devices
+    private static final int EXIT_CMD = -1;
+    private static final int KEY_RIGHT = 1;
+    private static final int KEY_LEFT = 2;
+
+    public ProcessConnectionThread(StreamConnection connection) {
+        mConnection = connection;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // prepare to receive data
+            InputStream inputStream = mConnection.openInputStream();
+            System.out.println("waiting for input");
+
+            while (true) {
+                int command = inputStream.read();
+
+                if (command == EXIT_CMD) {
+                    System.out.println("finish process");
+                    break;
+                }
+                processCommand(command);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Process the command from client
+     *
+     * @param command the command code
+     */
+    private void processCommand(int command) {
+        try {
+            switch (command) {
+                case KEY_RIGHT:
+                    System.out.println("Right");
+                    break;
+                case KEY_LEFT:
+                    System.out.println("Left");
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
